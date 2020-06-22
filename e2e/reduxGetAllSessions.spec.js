d78a53d970b38cbbe81934c81070db7ce0e3aaba
Tony Lomax <antl.lomax@gmail.com>
2020-06-22 15:26:13 +0100

    Detox test set up to check existence of sessions flatlist

---
+++ e2e/reduxGetAllSessions.spec.js
@@ -0,0 +1,13 @@
+describe('Check that Redux can subscribe to changing data in Firestore', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+  it('Should update the app screen with live session data from Firestore', async () => {
+    //do test stuff here
+    await expect(element(by.id('SessionsList'))).toExist();
+
+    // get id of flatList, check to see if there is either a Text tag that says no
+    // sessions or at least one session with a list of attenddes, a location, a formateed date tiem
+    // and a description
+  });
+});
