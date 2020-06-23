7fb28a676d3fed41d2d2c8b6b0d90ad4c0aaf53f
Jack Bridger <jacksbridger@gmail.com>
2020-06-23 10:38:04 +0100

    update test and pass

--- e2e/testNavigation.spec.js
+++ e2e/testNavigation.spec.js
@@ -5,10 +5,10 @@ describe('Example', () => {
   const exampleEmail = 'test@test.com';
   const examplePassword = '!wqwr#rewrew231';
 
-  it('it should be able to login, be redirected to profile page and navigate to settings page', async () => {
+  it('it should be able to login, be redirected to profile page', async () => {
     await element(by.id('email')).typeText(exampleEmail);
     await element(by.id('password')).typeText(examplePassword);
     await element(by.id('submit-login-details')).tap();
-    await element(by.id('upcoming-sessions-title'));
+    await expect(element(by.id('upcoming-sessions-title'))).toExist();
   });
 });
