de31fe515b2ff2776b56faf3acad92c16c00847b
Tony Lomax <antl.lomax@gmail.com>
2020-06-24 13:36:05 +0100

    Create shared components with test

---
+++ src/components/RegisterTabs.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export default function RegisterTabs({title}) {
+  return <Text> I'll be some kind of tabs at some point </Text>;
+}
---
+++ src/components/cards/PersonCardSession.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function PersonCardSession({title}) {
+  return (
+    <View>
+      <Text> I am a person</Text>
+    </View>
+  );
+}
