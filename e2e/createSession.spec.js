4a87f0be47933fa82adcc86212049c542bc75451
Tony Lomax <antl.lomax@gmail.com>
2020-06-26 12:11:41 +0100

    Fix test and push.

--- e2e/createSession.spec.js
+++ e2e/createSession.spec.js
@@ -85,8 +85,13 @@ describe('Create a surf session', () => {
       await element(by.id('email')).typeText(exampleEmail);
       await element(by.id('password')).typeText(examplePassword);
       await element(by.id('submit-login-details')).tap();
-      await expect(element(by.id('navigate-to-create-session'))).toExist();
+
+      waitFor(element(by.id('navigate-to-create-session')))
+        .toExist()
+        .withTimeout(5000);
+
       await element(by.id('navigate-to-create-session')).tap();
+
       await expect(element(by.id('create-session-title'))).toExist();
       await expect(element(by.id('type-of-session'))).toExist();
 
