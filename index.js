96492938a5e6793cb7b0ee5625a3bf254bcb248c
Tony Lomax <antl.lomax@gmail.com>
2020-07-22 11:29:30 +0100

    Attempt setup for ios notifications.

--- index.js
+++ index.js
@@ -7,16 +7,29 @@ import App from './src/App';
 import {name as appName} from './app.json';
 import {Provider as ReduxProvider} from 'react-redux';
 import {Provider as PaperProvider} from 'react-native-paper';
-
 import store from './src/redux/store';
 import React from 'react';
+import messaging from '@react-native-firebase/messaging';
 
 console.disableYellowBox = true;
+
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  console.log('Message handled in the background!', remoteMessage);
+});
+
+const HeadlessCheck = ({isHeadless}) => {
+  if (isHeadless) {
+    // App has been launched in the background by iOS, ignore
+    return null;
+  }
+
+  return <App />;
+};
 
 const AppWithReduxProvider = () => (
   <ReduxProvider store={store}>
     <PaperProvider>
-      <App />
+      <HeadlessCheck />
     </PaperProvider>
   </ReduxProvider>
 );
