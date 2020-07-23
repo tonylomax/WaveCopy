db0ac62bd480017790920a8dfc8a82643755e8fa
Tony Lomax <antl.lomax@gmail.com>
2020-07-23 10:00:37 +0100

    Resolve signout permission error

--- src/utils/auth/signOut.js
+++ src/utils/auth/signOut.js
@@ -7,12 +7,19 @@ import {
 import store from './../../redux/store';
 
 export default async function signOut() {
-  return await auth()
-    .signOut()
-    .then(() => {
-      store.dispatch(updateRoleSpecificSessions([]));
-      store.dispatch(updateSessions([]));
-      store.dispatch(updateFirestoreUserData({}));
-      // console.log('User signed out!');
-    });
+  return new Promise(async (resolve, reject) => {
+    store.dispatch(updateRoleSpecificSessions([]));
+    store.dispatch(updateSessions([]));
+    store.dispatch(updateFirestoreUserData({}));
+    return await auth()
+      .signOut()
+      .then(() => {
+        console.log('User signed out!');
+        resolve('User signed out');
+      })
+      .catch((err) => {
+        console.log('Signout Err', err);
+        reject(err);
+      });
+  });
 }
